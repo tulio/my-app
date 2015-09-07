7d387e6a7004ee4c5d9e11b084040c97fe21db2f
Túlio Freitas <tuliofreitas@live.com>
2015-09-07 02:39:17 -0300

    Creating home template and controller

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home'
+]).
+config(['$routeProvider', function($routeProvider) {
+  // Routes will be here
+  $routeProvider.otherwise({
+  	redirectTo: '/home'
+  });
+}]);
